9def3eb94123b2c6703e555c95071541ee573d59
Mervin Anthony <anthony.mervin@gmail.com>
2026-06-11 19:27:44 +0000

    v44: debug info in member modal + bump sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Score4Fun Service Worker — network-first pour toujours servir la dernière version
-const CACHE = 's4f-v43';
+const CACHE = 's4f-v44';
 
 self.addEventListener('install', () => self.skipWaiting());
 
