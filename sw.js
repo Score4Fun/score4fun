07e2c35d17f26604b046f30beb3c03f41770e5bd
Mervin <anthony.mervin@gmail.com>
2026-06-13 05:18:37 +0000

    v76: news — Kane 36/31, Toney 32/32, 2nd behind Ronaldo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Score4Fun Service Worker — network-first pour toujours servir la dernière version
-const CACHE = 's4f-v75';
+const CACHE = 's4f-v76';
 
 self.addEventListener('install', () => self.skipWaiting());
 
